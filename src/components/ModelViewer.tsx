9f611a4156d41422d38b876c76783bfdd0db9cb0
Ovik <47368700+Ovikx@users.noreply.github.com>
2022-06-15 02:57:14 -0400

    added the city's buildings

--- src/components/ModelViewer.tsx
+++ src/components/ModelViewer.tsx
@@ -2,6 +2,9 @@ import React, { useRef, useState, useEffect } from 'react';
 import { Canvas, useFrame, useThree } from '@react-three/fiber';
 import * as THREE from 'three';
 
+const basePlateLength = 5;
+const subdivisions = 25;
+
 function Box (props: JSX.IntrinsicElements['mesh']) {
     const mesh = useRef<THREE.Mesh>(null!);
 
@@ -26,19 +29,73 @@ function Box (props: JSX.IntrinsicElements['mesh']) {
     );
 }
 
+function CityBase (props: JSX.IntrinsicElements['mesh']) {
+    const mesh = useRef<THREE.Mesh>(null!);
+
+    return (
+        <mesh
+            {...props}
+            ref={mesh}
+            position={[0,0,0]}
+        >
+            <boxGeometry args={[basePlateLength,0.25,basePlateLength]} />
+            <meshStandardMaterial color={'dimgray'} />
+        </mesh>
+    );
+}
+
+function Buildings (props: JSX.IntrinsicElements['mesh']) {
+    const mesh = useRef<THREE.Mesh>(null!);
+    const buildings = [];
+    let counter = 0;
+    for (let i = 1; i < subdivisions; i++) {
+        for (let j = 1; j < subdivisions; j++) {
+            const x = i*(basePlateLength/subdivisions)-(basePlateLength/2);
+            const y = 0.25;
+            const z = j*(basePlateLength/subdivisions)-(basePlateLength/2);
+            const height = Math.random()*2;
+            const width = basePlateLength/(subdivisions*1.5)
+
+            buildings.push(
+                <mesh
+                    {...props}
+                    ref={mesh}
+                    position={[x,y,z]}
+                    key={counter}
+                >
+                        <boxGeometry args={[width, height, width]} />
+                </mesh>
+            );
+            counter++;
+            console.log(`added mesh at: ${x}, ${y}, ${z}`);
+        }
+    }
+    return (
+        <mesh
+            {...props}
+            ref={mesh}
+        >
+            {buildings}
+        </mesh>
+
+    )
+}
+
 function City (props: JSX.IntrinsicElements['mesh']) {
     const mesh = useRef<THREE.Mesh>(null!);
-    useFrame((state, delta) => mesh.current.rotation.y += 0.0025);
+    useFrame((state, delta) => {
+        mesh.current.rotation.y += 0.0025;
+    })
 
     return (
         <mesh
             {...props}
             ref={mesh}
         >
-            <boxGeometry args={[5,0.25,5]} />
-            <meshStandardMaterial color={'dimgray'} />
+            <CityBase />
+            <Buildings />
         </mesh>
-    );
+    )
 }
 
 function Camera () {
@@ -54,7 +111,7 @@ export const ModelViewer = () => {
         <Canvas style={{height: '500px'}}>
             <ambientLight />
             <pointLight position={[10,10,10]} />
-            <City position={[0,0,0]} />
+            <City />
             <Camera />
         </Canvas>
     );
